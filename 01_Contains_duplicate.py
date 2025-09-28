2dc210f74b753b8e98ba06486af4977288876dd3
monjurulhaquerajun <monjurulhaquerajun@gmail.com>
2025-09-28 23:07:26 +0600

    contains duplicate

---
+++ 01_Contains_duplicate.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                return True
+        return False
+
